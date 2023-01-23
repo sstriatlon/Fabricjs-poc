af5574352ad228da7ec8b62778659210e4f35e55
SANTIAGO SUAREZ <sstriatlon@gmail.com>
2023-01-23 00:20:42 -0300

    Resize of circles with konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
 import { Todo } from './model'
-import { Circle, Layer, Line, Rect, Stage, Text } from 'react-konva'
+import { Circle, Layer, Line, Rect, Stage, Text, Transformer, Image } from 'react-konva'
 
 
 interface circulo {
@@ -30,9 +30,86 @@ function generateShapes(){
 
 const INITIAL_STATE = generateShapes()
 
+type test = {
+
+}
+const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }) => {
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current?.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  return (
+    <React.Fragment>
+      <Circle
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
 const App: React.FC = () => {
 
   const [formas ,setFormas]=useState<Array<circulo>>(INITIAL_STATE);
+  const [selectedId, selectShape] = React.useState<string | null>(null);
+
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
 
   const handleDragStart = (e) => {
     const id = e.target.id();
@@ -58,21 +135,36 @@ const App: React.FC = () => {
 
  return (
     <div className="App">
-        <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Stage 
+        width={window.innerWidth} 
+        height={window.innerHeight}
+        onMouseDown={checkDeselect}
+        onTouchStart={checkDeselect}
+      >
           <Layer>
-          
-            {formas?.map((obj)=>{
-              return <Circle
-              id={obj.id} 
-              x={obj.x} 
-              y={obj.y} 
-              radius={obj.radius} 
-              fill={obj.fill} 
-              isDragging={obj.isDragging}
+            {formas.map((obj, rect)=>{
+              return <Rectangle
+              shapeProps={obj}
+              isSelected={obj.id === selectedId}
+              onSelect={() => {
+                selectShape(obj.id);
+              }}
+              onChange={(newAttrs) => {
+                const rects = formas.slice();
+                rects[rect] = newAttrs;
+                setFormas(rects);
+                console.log(newAttrs);
+              }}
+              // id={obj.id} 
+              // x={obj.x} 
+              // y={obj.y} 
+              // radius={obj.radius} 
+              // fill={obj.fill} 
+              // isDragging={obj.isDragging}
               key={obj.id}
-              onDragStart={handleDragStart}
-              onDragEnd={handleDragEnd}
-              draggable
+              // onDragStart={handleDragStart}
+              // onDragEnd={handleDragEnd}
+              //draggable
             />
             })}
 
