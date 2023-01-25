f7e21394573fd060ab8993f3d7db6c0ef3457ce3
SANTIAGO SUAREZ <sstriatlon@gmail.com>
2023-01-25 16:37:12 -0300

    Fabricjs POC
    IMplementation of canvas and a button for adding rectangles or an input for adding images and play with them in a canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { Todo } from './model'
-import { Circle, Layer, Line, Rect, Stage, Text } from 'react-konva'
+import { fabric } from 'fabric';
+
+
 
 
 interface circulo {
@@ -31,53 +32,60 @@ function generateShapes(){
 const INITIAL_STATE = generateShapes()
 
 const App: React.FC = () => {
+  const [canvas, setCanvas] = useState<fabric.Canvas>();
+  const [imgURL, setImgURL] = useState<string>("");
 
-  const [formas ,setFormas]=useState<Array<circulo>>(INITIAL_STATE);
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, []);
 
-  const handleDragStart = (e) => {
-    const id = e.target.id();
-    setFormas(
-      formas.map((star) => {
-        return {
-          ...star,
-          isDragging: star.id === id,
-        };
-      })
-    );
-  };
-  const handleDragEnd = (e) => {
-    setFormas(
-      formas.map((star) => {
-        return {
-          ...star,
-          isDragging: false,
-        };
-      })
-    );
-  };
+  const initCanvas = () => (
+    new fabric.Canvas('canvas', {
+       height: 800,
+       width: 800,
+       backgroundColor: 'pink'
+    })
+ );
+ 
+const addRect = (canvi: fabric.Canvas) => {
+  const rect = new fabric.Rect({
+    height: 100,
+    width: 100,
+    fill: 'yellow'
+  });
+  canvi.add(rect);
+  canvi.renderAll();
+};
+
+const addImg = (e: React.FormEvent<HTMLFormElement>,
+   url: string, canvi: fabric.Canvas) => {
+  e.preventDefault();
+  
+  new fabric.Image.fromURL(url, img => {
+    img.scale(0.25);
+    canvi.add(img);
+    canvi.renderAll();
+    setImgURL('');  
+  });
+}
 
  return (
     <div className="App">
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-          
-            {formas?.map((obj)=>{
-              return <Circle
-              id={obj.id} 
-              x={obj.x} 
-              y={obj.y} 
-              radius={obj.radius} 
-              fill={obj.fill} 
-              isDragging={obj.isDragging}
-              key={obj.id}
-              onDragStart={handleDragStart}
-              onDragEnd={handleDragEnd}
-              draggable
+        <h1>Fabric.js on React - fabric.Canvas('...')</h1>
+        <button onClick={() => addRect(canvas)}>Rectangle</button>
+        <br/><br/>
+        <form onSubmit={e => addImg(e, imgURL, canvas)}>
+          <div>
+            <input 
+                type="text" 
+                value={imgURL} 
+                onChange={ e => setImgURL(e.target.value)} 
             />
-            })}
-
-          </Layer>
-        </Stage>
+            <button type="submit">Add Image</button>
+          </div>
+        </form>
+        <br/><br/>
+        <canvas id="canvas"/>
     </div>
   )
 }
--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,11 @@
+// namespace OrderModule {
+//     export class Order { /* … */ }
+//     export function cancelOrder(order: Order) { /* … */ }
+//     export function processOrder(order: Order) { /* … */ }
+//    }
+//    let order = new OrderModule.Order();
+//    OrderModule.cancelOrder(order);
+
 export interface Todo {
     id: number;
     todo:string;
